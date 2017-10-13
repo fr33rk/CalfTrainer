314ea560ee97e2e4df55af9c871c1e1bfa0332ea
Freerk de Leeuw <freerk@deleeuw.frl>
2017-10-13 10:24:56 +0200

    Added classes for exercise tracking.

---
+++ CalfTrainer.Android/PL.CalfTrainer.Infrastructure/EventArgs/DailyExerciseTrackerChangedArgs.cs
@@ -0,0 +1,14 @@
+﻿using PL.CalfTrainer.Entities;
+
+namespace PL.CalfTrainer.Infrastructure.EventArgs
+{
+	public class DailyExerciseTrackerChangedArgs : System.EventArgs
+	{
+		public DailyExerciseTrackerChangedArgs(DailyExerciseTracker dailyExerciseTracker)
+		{
+			DailyExerciseTracker = dailyExerciseTracker;
+		}
+
+		public DailyExerciseTracker DailyExerciseTracker { get; }
+	}
+}
---
+++ PL.CalfTrainer.Business/Services/ExerciseTrackerService.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="ExerciseTrackerService.cs" company="Delta Instruments">Copyright © Delta Instruments B.V.</copyright>
+using System;
+using System.Collections.Generic;
+using PL.CalfTrainer.Entities;
+using PL.CalfTrainer.Infrastructure.EventArgs;
+using PL.CalfTrainer.Infrastructure.Services;
+
+namespace PL.CalfTrainer.Business.Services
+{
+
+
+	public class ExerciseTrackerService : IExerciseTrackerService
+	{
+		private IExerciseTrackerDataService mDataService;
+
+		public ExerciseTrackerService(IExerciseTrackerDataService dataService)
+		{
+			mDataService = dataService;
+		}
+
+		public void ExerciseFinished(Exercise exercise)
+		{
+			// Test how far the test is finished
+			// Create a ExerciseResult object and store it.
+			throw new NotImplementedException();
+		}
+
+		public DailyExerciseTracker GetExecutionsOfDay(DateTime day)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IList<DailyExerciseTracker> GetExecutionsOfPeriod(DateTime startOfPeriod, DateTime endOfPeriod)
+		{
+			throw new NotImplementedException();
+		}
+
+		public event EventHandler<DailyExerciseTrackerChangedArgs> DailyExerciseTrackerChanged;
+	}
+}
